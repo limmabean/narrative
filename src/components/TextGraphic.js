36f8bc952249c407e6f147b9fe559aca5406b465
Matthew Lim <limmat@gatech.edu>
2021-09-22 15:10:28 -0400

    Added font.

--- src/components/TextGraphic.js
+++ src/components/TextGraphic.js
@@ -1,33 +1,35 @@
 import React from "react"
 import Sketch from "react-p5";
-import "@fontsource/titillium-web"
-
-let x = 50;
-let y = 50;
-
+import {Col, Row} from "react-flexbox-grid";
+import {Button, FrameBox, FramePentagon, Text} from "@arwes/core";
 
 
 const TextGraphic = (props) => {
   const words = [] // store word objects
   let selectedWord = null;
   let selectedOffset = { x: 0, y: 0 }
-
+  const initialX = 10;
+  const initialY = 30;
   const preload = (p5) => {
-    p5.fontRegular = p5.loadFont("titillium-web");
+    console.log("hello");
+    p5.fontRegular = p5.loadFont("/static/fonts/TitilliumWeb-Regular.ttf");
+    p5.fontItalic = p5.loadFont("/static/fonts/TitilliumWeb-Italic.ttf");
+    p5.fontBold = p5.loadFont("/static/fonts/TitilliumWeb-Bold.ttf");
   }
   const setup = (p5, canvasParentRef) => {
     // use parent to render the canvas in this ref
     // (without that p5 will render the canvas outside of your component)
-    p5.createCanvas(1184, 400).parent(canvasParentRef);
-    p5.background(0)
+    p5.createCanvas(1164, 400).parent(canvasParentRef);
+    p5.background(0, 60, 63)
     updateWords(p5);
   };
 
   const updateWords = (p5) => {
     p5.textSize(24)
+    p5.textFont(p5.fontRegular);
     // track word position
-    let x = 20
-    let y = 60
+    let x = initialX;
+    let y = initialY;
     p5.fill(255)
     let wordsStr = [];
     props.selectedText.text.forEach((txtSegment) => {
@@ -48,15 +50,16 @@ const TextGraphic = (props) => {
         x = x + wordStrWidth + p5.textWidth(' ') // update x by word width + space character
         // look ahead the next word - will it fit in the space? if not, line break
         const nextWordStrWidth = p5.textWidth(wordsStr[i+1]) || 0
-        if (x > p5.width - nextWordStrWidth) {
+        if (x > (p5.width - initialX) - nextWordStrWidth) {
           y += p5.textAscent(words[0].word) + 10 // line height, sort of
-          x = 20 // reset x position
+          x = initialX // reset x position
         }
       }
     })
+    p5.resizeCanvas(1164, y + 20)
   }
   const draw = (p5) => {
-    p5.background(0);
+    p5.background(0, 60, 63);
     for (let i = 0; i < words.length; i++) {
       const word = words[i] // retrieve word object
       if (word !== selectedWord) {
@@ -70,7 +73,7 @@ const TextGraphic = (props) => {
   };
 
   const keyPressed = (p5) => {
-    if (p5.key === 'r') {
+    if (p5.key === 'r' && !props.constant) {
       for (let word of words) word.spread()
     } else if (p5.key === ' ') {
       for (let word of words) word.reset()
@@ -178,7 +181,7 @@ const TextGraphic = (props) => {
             const wordStrWidth = p5.textWidth(words[i].word); // get current word width
             if (x + wordStrWidth > p5.width) {
               y += p5.textAscent(words[0].word)
-              x = 20;
+              x = initialX;
             }
 
             words[i].tx = x;
@@ -191,7 +194,7 @@ const TextGraphic = (props) => {
             const nextWordStrWidth = p5.textWidth(words[i+1]) || 0
             if (x > p5.width - nextWordStrWidth) {
               y += p5.textAscent(words[0].word) + 10// line height, sort of
-              x = 20 // reset x position
+              x = initialX // reset x position
             } else {
               y = y;
             }
@@ -202,8 +205,45 @@ const TextGraphic = (props) => {
     }
   }
 
-  return <Sketch preload={preload} setup={setup} draw={draw} keyPressed={keyPressed} mousePressed={mousePressed}
-                 mouseReleased={mouseReleased}/>;
+  return (
+      <>
+        <Row key={props.selectedText.key}>
+          <Row style={{paddingTop: "20px"}}>
+            <Col>
+              <FramePentagon style={{width: "1184px"}}>
+                <div>
+                  <Text as={"h4"} style={{marginBottom: "0px"}}>Origin: {props.selectedText.origin}</Text>
+                  <br/>
+                  <Text as={"span"}>Encryption: {props.selectedText.encryption}</Text>
+                </div>
+              </FramePentagon>
+            </Col>
+          </Row>
+          <Row style={{paddingTop: "20px"}}>
+            <Col>
+              <FramePentagon style={{width: "1184px"}}>
+                <div>
+                  <Text as={"h4"}  style={{marginBottom: "0px"}}>Destination: {props.selectedText.dest}</Text>
+                </div>
+              </FramePentagon>
+            </Col>
+          </Row>
+          <Row style={{paddingTop: "20px"}}>
+            <Col>
+              <FrameBox>
+                <Sketch preload={preload} setup={setup} draw={draw} keyPressed={keyPressed} mousePressed={mousePressed}
+                        mouseReleased={mouseReleased}/>
+              </FrameBox>
+            </Col>
+          </Row>
+          {!props.selectedText.constant &&
+            <Row style={{paddingTop: "20px"}} end={"xs"} >
+              <Button palette={"primary"} FrameComponent={FramePentagon}><Text>Send</Text></Button>
+            </Row>
+          }
+        </Row>
+      </>
+  );
 };
 
 export default TextGraphic
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,57 +1,82 @@
-import React from "react"
-import {ArwesThemeProvider, Button, FrameBox, FramePentagon, StylesBaseline, Text} from "@arwes/core";
+import React, { useState } from "react"
+import {ArwesThemeProvider, Button, FrameBox, FrameLines, StylesBaseline, List, FrameHexagon, Text} from "@arwes/core";
+import Sketch from "react-p5";
+
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import TextGraphic from '../components/TextGraphic'
 import script from "../script.json"
 import "@fontsource/titillium-web";
 
 const duration = { enter: 1000, exit: 1000 };
-
-function changeWords(selectedText) {
-  selectedText = "Changed this word"
-}
-
+let x = 50;
+let y = 50;
+let angle = 0;
+let panicLevel = 10;
+let panicLevelCurrent = 0;
 // markup
 const IndexPage = () => {
-  let messages = [];
-  function pushMessage(message) {
-    messages.push(message);
+  const panicSetup = (p5, canvasParentRef) => {
+    // use parent to render the canvas in this ref
+    // (without that p5 will render the canvas outside of your component)
+    p5.createCanvas(120, 120, p5.WEBGL).parent(canvasParentRef);
+  };
+
+  const panicUp = () => {
+    if (panicLevel < 10) panicLevel++;
   }
+  const panicDown = () => {
+    if (panicLevel > 0) panicLevel--;
+  }
+
+  const panicDraw = (p5) => {
+    p5.background(0, 60, 63);
+    if (panicLevel >= 7) {
+      if (panicLevelCurrent < panicLevel) {
+        const redLevel = (255) * (panicLevel/10) * (panicLevelCurrent/panicLevel);
+        const greenLevel = 255 - (255 * panicLevelCurrent/panicLevel);
+        p5.fill(redLevel,greenLevel,0)
+        panicLevelCurrent += .1;
+      }
+    } else if (panicLevel < 7) {
+      if (panicLevelCurrent > panicLevel) {
+        const redLevel = (255) - (255 * ((10 - panicLevelCurrent)/(10 - panicLevel)));
+        const greenLevel = (255 * ((10-panicLevel)/10) * ((panicLevelCurrent)))
+        p5.fill(redLevel,greenLevel,0);
+      }
+      p5.fill(0,255,0);
+    }
+    p5.translate (10, 10, -200);
+    p5.noStroke();
+    p5.rotateX(angle);
+    p5.rotateY(angle * 1.3);
+    p5.rotateZ(angle * 0.7);
+    p5.box(100);
+    angle += 0.03;
+  };
+
+  const [messages, setMessages] = useState(script.messages);
 
   return (
       <ArwesThemeProvider>
         <StylesBaseline styles={{body: { fontFamily: "Titillium Web" }}}/>
         <main>
           <title>Home Page</title>
+          <div style={{position: "fixed", top: "20px"}}>
+            <FrameBox>
+              <Sketch setup={panicSetup} draw={panicDraw} />
+            </FrameBox>
+            <br/>
+            <Button onClick={panicUp}>up</Button>
+            <Button onClick={panicDown}>down</Button>
+          </div>
           <Grid>
-            <Row>
-              <Col>
-                <FrameBox>
-                  <Text as={"h4"}>Origin: {script.messages[0].origin}</Text>
-                  <br/>
-                  <Text as={"h4"}>Destination: {script.messages[0].dest}</Text>
-                  <br/>
-                  <Text as={"span"}>Encryption: {script.messages[0].encryption}</Text>
-                </FrameBox>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <FrameBox>
-                  <TextGraphic selectedText={script.messages[0]}></TextGraphic>
-                </FrameBox>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <FrameBox>
-                  <TextGraphic selectedText={script.messages[0]}></TextGraphic>
-                </FrameBox>
-              </Col>
-            </Row>
-            <Row>
-            </Row>
+            { messages.map((message) => {
+              return (
+                    <TextGraphic selectedText={message} constant={message.constant}></TextGraphic>
+                  )
+            })}
           </Grid>
+
         </main>
       </ArwesThemeProvider>
   )
